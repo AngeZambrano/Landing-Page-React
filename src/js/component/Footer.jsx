711529ffb5215df803066e75ecef321a6b9f4172
AngeZambrano <129020174+AngeZambrano@users.noreply.github.com>
2023-06-14 12:33:44 +0000

    First commit

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (<div className="container-fluid bg-dark w-75 p-3">
+        <h4>CopyRights</h4>
+    </div> );
+}
+
+export default Footer;
